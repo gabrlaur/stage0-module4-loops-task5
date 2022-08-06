0c2fc9402bc7919e07c6600922e046a69210ca30
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-08-06 20:14:34 +0300

    Task #5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,40 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int middle = height / 2;
+        int needToPrint;
+        int pastMid = middle-1;
+        boolean isEven = height % 2 == 0;
+        if (height == 0) {
+            return;
+        }
+        int evenHeight = height;
+        if (isEven) {
+            evenHeight = height + 1;
+        }
+        for (int i = 0; i < evenHeight; i++) {
+            needToPrint = i <= middle ? height - i * 2 : height - pastMid * 2;
+            if (needToPrint == 0) {
+                continue;
+            }
+            if (needToPrint < height) {
+                for (int j = 0; j < (height - needToPrint) / 2; j++) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < needToPrint; j++) {
+                System.out.print("8");
+            }
 
+            if (needToPrint < height) {
+                for (int j = 0; j < (height - needToPrint) / 2; j++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            if (i > middle) {
+                pastMid--;
+            }
+        }
     }
 }
